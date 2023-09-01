34d4b33fd305c7797cc0be7a78d42c18ade9157d
Ivan Serrano <ramiroivanserrano@gmail.com>
2023-09-01 15:26:51 -0300

    primer commit

---
+++ SistemaCNE/src/main/java/com/unsepui/sistemacne/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.unsepui.sistemacne;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
